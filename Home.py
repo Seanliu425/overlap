541cebbb4b983000c8bc0d3f54ce25b330d9f934
seanliu425 <seanliu0425@gmail.com>
2024-04-26 11:48:47 -0500

    homepage additions

--- Home.py
+++ Home.py
@@ -27,5 +27,18 @@ st.markdown(
 
     Our graphs are hosted using Ouestware's [Retina](https://ouestware.gitlab.io/retina/beta/#/) tool.
 
+    ### How do I read Social Network Graphs?
+    Our networks aim to visualize collaboration within Chicago OST networks by leveraging access to multiple city-wide datasets and 
+    demonstrating webs of collaboration that may not be immediately visible. 
+    
+    The nodes in our graphs represent afterschool providers, community organizations, schools, and program locations, amongst others. A link between nodes represents reported collaboration between the two nodes — this could represent a provider reporting they work at a location, a school reporting that they work with a provider, or a provider working with another provider. 
+    ***More than anything, these graphs should be understood as depictions of collaboration — a map to help pave future collaborations.*** 
+
+    In many of our graphs, the nodes are sized by ***Degree***, a measure of how many links that each node has. If a graph is dominated by a select few nodes that contain the majority of the links, this is known as a ***Scale-Free Network***
+    
+    The graph format is arranged using a force-repulsion algorithm, meaning that nodes repel each other, but \
+    those with links between them attract each other. The resulting image depicts hubs of collaboration between organizations: \
+    nodes that are close in proximity are likely, but not guaranteed, to be connected. The layout of the graphs are structured to facillitate easy identification of collaboration between nodes, but the physical location of each node does not have any specific meaning.
+
 """
 )
